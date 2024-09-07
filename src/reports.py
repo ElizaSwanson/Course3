242a4147004f84cb1e31509158dfcc2eb8fbff2a
Elizaveta Nasytko <Liz99246@gmail.com>
2024-09-07 21:27:10 +0300

    Работает reports

---
+++ src/reports.py
@@ -0,0 +1,78 @@
+import datetime
+import datetime as dt
+import logging
+from functools import wraps
+from typing import Any, Callable
+
+import pandas as pd
+
+from src.utils import get_data, reading_excel
+
+logger = logging.getLogger("logs")
+logger.setLevel(logging.INFO)
+reports_handler = logging.FileHandler("..\\logs\\reports.log", encoding="utf-8")
+repots_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+reports_handler.setFormatter(repots_formatter)
+logger.addHandler(reports_handler)
+
+
+def decorator_log(filename: Any = None) -> Callable:
+    """Декоратор логирует вызов функции и результат вызова"""
+
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write("\n my function is OK")
+                else:
+                    print("\n my function is OK")
+            except Exception as e:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(
+                            f"\n my function error: {e}, inputs: {args}, {kwargs} \n"
+                        )
+                else:
+                    print(f"\n my function error: {e}, inputs: {args}, {kwargs} \n")
+            return wrapper
+
+        return wrapper
+
+    return decorator
+
+
+def spending_by_category(
+    df_transactions: pd.DataFrame, category: str, date: [str] = None
+) -> pd.DataFrame:
+    """Функция возвращает траты по категории за последние три месяца. Если дата не указана, берется текущая"""
+    if date is None:
+        picked_data = dt.datetime.now()
+    else:
+        picked_data = get_data(date)
+    earlier_data = picked_data.replace(hour=0, minute=0, second=0) - datetime.timedelta(
+        days=91
+    )
+    transactions_by_category = df_transactions.loc[
+        (pd.to_datetime(df_transactions["Дата операции"], dayfirst=True) <= picked_data)
+        & (
+            pd.to_datetime(df_transactions["Дата операции"], dayfirst=True)
+            >= earlier_data
+        )
+        & (df_transactions["Категория"] == category)
+    ]
+
+    trans_sum = sum(transactions_by_category["Сумма операции с округлением"])
+    return f"{transactions_by_category}, итого потрачено в данной категории за выбранный период: {trans_sum}"
+
+
+if __name__ == "__main__":
+    result = spending_by_category(
+        reading_excel("..\\data\\operations.xlsx"),
+        "Супермаркеты",
+        "01.01.2021 00:00:00",
+    )
+    print(result)
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
-import os
-import pandas as pd
-import json
+import datetime
 import logging
+import os
+
+import pandas as pd
 
 path_to_log = "logs/utils.log"
 abs_path = os.path.abspath(path_to_log)
@@ -16,24 +17,40 @@ logger_utils.setLevel(logging.DEBUG)
 
 
 def reading_excel(path_to_file: str) -> list[dict]:
+    logger_utils.info(f"Пытаюсь прочитать файл {path_to_file}")
     try:
         df_list = pd.read_excel(path_to_file)
+        logger_utils.info("Считано!")
         return df_list
     except FileNotFoundError:
+        logger_utils.error("Нельзя прочитать то, чего нет!")
         raise
+
 
 def get_dict_transaction(file_path) -> list[dict]:
     """Функция преобразовывающая датафрейм в словарь pyhton"""
-    logger_utils.info(f"Вызвана функция get_dict_transaction с файлом {file_path}")
+
+    logger_utils.info(f"Начинаем преобразовывать {file_path}...")
     try:
         df = pd.read_excel(file_path)
         logger_utils.info(f"Файл {file_path}  прочитан")
         dict_transaction = df.to_dict(orient="records")
-        logger_utils.info("Датафрейм  преобразован в список словарей")
+        logger_utils.info("Список словарей создан")
         return dict_transaction
     except FileNotFoundError:
-        logger_utils.error(f"Файл {file_path} не найден")
+        logger_utils.error("Нельзя прочитать то, чего нет")
         raise
     except Exception as e:
         logger_utils.error(f"Произошла ошибка: {str(e)}")
         raise
+
+
+def get_data(data: str) -> datetime.datetime:
+    logger_utils.info(f"Получена дата: {data}")
+    try:
+        data_now = datetime.datetime.strptime(data, "%d.%m.%Y %H:%M:%S")
+        logger_utils.info("Дата преобразована")
+        return data_now
+    except Exception as e:
+        logger_utils.info(f"Ошибка: {e}")
+        return e
